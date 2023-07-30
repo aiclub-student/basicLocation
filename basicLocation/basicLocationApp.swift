e4099a9ed81642079ff891d162cf6355b8849977
Danika Gupta <danika@gprof.com>
2023-07-29 23:47:55 -0700

    Initial Commit

---
+++ basicLocation/basicLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicLocationApp.swift
+//  basicLocation
+//
+//  Created by Amit Gupta on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct basicLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
